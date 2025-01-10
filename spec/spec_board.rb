f58d9f3bf42b2f6ac73d92ba0925aabac8615f16
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-09 19:46:00 -0800

    Add methods for the board class

--- spec/spec_board.rb
+++ spec/spec_board.rb
@@ -3,4 +3,21 @@
 require_relative '../lib/board'
 
 describe Board do
+  subject(:board) { described_class.new }
+
+  context '#initialize' do
+    # Nothing to test
+  end
+
+  context '#update_board' do
+    # Updates board with current player's piece
+  end
+
+  context '#four_in_a_row?' do
+    # Checks if there's a four in a row
+  end
+
+  context '#tie?' do
+    # Check if the board is full
+  end
 end
