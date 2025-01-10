773f5fc241affe84422ab793ebc16c7e2f4db37b
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-09 19:25:26 -0800

    Add vars and init method to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
+require_relative 'player'
+require_relative 'board'
+
 class Game
+  attr_accessor :players, :current_player, :board
+
+  def initialize
+    @players = [Player.new, Player.new]
+    @current_player = 0
+    @board = Board.new
+  end
 end
