fb4bf07445c7e4767e64ed0fe8e538fe5ee95d0c
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-09 18:58:00 -0800

    Add describe game and nested describe methods
    Creating the outline of the methods I believe I'll need based on my
    brainstorming in README

--- spec/spec_game.rb
+++ spec/spec_game.rb
@@ -2,5 +2,18 @@
 
 require_relative '../lib/game'
 
-describe 'game' do
+describe Game do
+  subject(:game) { described_class.new }
+
+  describe '#initialize' do
+  end
+
+  describe '#take_turn' do
+  end
+
+  describe '#validate_input' do
+  end
+
+  describe 'game_over?' do
+  end
 end
