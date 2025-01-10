b9ffea12730747f44ef542e0e1ec572c61a6acb8
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-09 19:22:34 -0800

    Add require relative for player, comment to init

--- spec/spec_game.rb
+++ spec/spec_game.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/player'
 
 describe Game do
   subject(:game) { described_class.new }
@@ -8,6 +9,7 @@ describe Game do
   let(:player) { double('player') }
 
   describe '#initialize' do
+    # Nothing to test
   end
 
   describe '#take_turn' do
