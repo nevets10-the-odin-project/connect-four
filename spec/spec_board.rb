be14c2d95a3b8893f6be05a16199098aeb91ef3d
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-15 19:08:07 -0800

    Add second test for four_in_a_diagonal?

--- spec/spec_board.rb
+++ spec/spec_board.rb
@@ -150,6 +150,25 @@ describe Board do
 
       expect(board).to_not be_four_in_a_diagonal(selected_index, player_symbol)
     end
+
+    it 'returns true if there are four consecutive symbols in a diagonal; top right to bottom left' do
+      # I don't want to think harder than I have to for test setups
+      board.update_board(3, other_symbol)
+      board.update_board(3, player_symbol)
+      board.update_board(3, other_symbol)
+      board.update_board(3, player_symbol)
+
+      board.update_board(2, player_symbol)
+      board.update_board(2, other_symbol)
+      board.update_board(2, player_symbol)
+
+      board.update_board(1, other_symbol)
+      board.update_board(1, player_symbol)
+
+      board.update_board(0, player_symbol)
+
+      expect(board).to_not be_four_in_a_diagonal(selected_index, player_symbol)
+    end
   end
 
   context '#tie?' do
