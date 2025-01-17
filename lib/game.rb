d5adc433408ae738ce3aad991547298cb75bdb30
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-16 18:54:37 -0800

    Write #get_player_input to pass first test

--- lib/game.rb
+++ lib/game.rb
@@ -9,4 +9,18 @@ class Game
     @current_player = 0
     @board = Board.new
   end
+
+  def get_player_input
+    loop do
+      user_input = gets.chomp
+      validated_input = validate_input(user_input.to_i) if user_input.match?(/^\d+$/)
+      return validated_input - 1 if validated_input
+
+      puts 'Number must be from 1 to 7.'
+    end
+  end
+
+  def validate_input(input)
+    input if input.between?(1, 7)
+  end
 end
