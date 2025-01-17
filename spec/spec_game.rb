88b842a0d2c45dbd542747294da36a5da8014e23
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-16 18:39:09 -0800

    Change describe to context

--- spec/spec_game.rb
+++ spec/spec_game.rb
@@ -8,11 +8,11 @@ describe Game do
   let(:board) { double('board') }
   let(:player) { double('player') }
 
-  describe '#initialize' do
+  context '#initialize' do
     # Nothing to test
   end
 
-  describe '#take_turn' do
+  context '#take_turn' do
     # Puts which player's turn it is.
     # Waits for player input
     # Validates input
@@ -20,11 +20,11 @@ describe Game do
     # Checks for game over
   end
 
-  describe '#validate_input' do
+  context '#validate_input' do
     # Confirms that the player input is valid
   end
 
-  describe 'game_over?' do
+  context 'game_over?' do
     # Checks if a player has won or if it's a tie
   end
 end
